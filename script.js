6540044db4ec4993a9cd3d68d29f3bc743ad7a9d
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-08 15:17:01 -0400

    able to target the input text

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+$(document).ready(function(){
+    
+	$("#new-message-button").on("click", function(){
+        msg = $("#new-message-body").val();
+    	// alert(msg);
+    	$("#conversation").append(msg);
+    });
 
+
+});
