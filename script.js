28db9103c13aae3a5859e5b2e3f9463360db75c9
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-08 19:03:12 -0400

    set time to it's own function

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 
 $(document).ready(function(){
 
-	function send() {
+	function timefy() {
 		var time = (new Date);
 		var h = time.getHours();
-		var m = (time.getMinutes()<10?'0':'') + time.getMinutes()
-		var str = h + ":" + m
+		var m = (time.getMinutes()<10?'0':'') + time.getMinutes();
+		var answer = h + ":" + m;
+		return answer;
+	}
+
+	function send() {
         var msg = $("#new-message-body").val();
-    	// alert(msg);
-    	$(".chat").append('<ol id="conversation">' + '<li class="message">' + '<a class="delete" href="#">Delete</a>' + '<h3 class="author">Me</h3>' + '<p class="message-body">' + msg + '</p>' + '<span class="timestamp">' + str + '</span>' + '</li>' + '</ol>');
+    	$(".chat").append('<ol id="conversation">' + '<li class="message">' + '<a class="delete" href="#">Delete</a>' + '<h3 class="author">Me</h3>' + '<p class="message-body">' + msg + '</p>' + '<span class="timestamp">' + timefy() + '</span>' + '</li>' + '</ol>');
     	$('#new-message-body').val("");
 	}
     
 	$("#new-message-button").on("click", send);
+
+	// $("textarea").keypress(function (e) {
+	// 	if (e.which == 13) {
+	// 		send;
+	// 		return false;
+	// 	}
+	// });
 
 	// $("#conversation a").on("click", function(){
  //    	console.log("X was clicked");
