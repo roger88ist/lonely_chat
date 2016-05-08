7456817210726675125da71c187f8da2d840b090
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-08 18:44:14 -0400

    refactored jquery code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+
 $(document).ready(function(){
-    
-	$("#new-message-button").on("click", function(){
+
+	function send() {
 		var time = (new Date);
 		var h = time.getHours();
 		var m = (time.getMinutes()<10?'0':'') + time.getMinutes()
@@ -9,7 +10,12 @@ $(document).ready(function(){
     	// alert(msg);
     	$(".chat").append('<ol id="conversation">' + '<li class="message">' + '<a class="delete" href="#">Delete</a>' + '<h3 class="author">Me</h3>' + '<p class="message-body">' + msg + '</p>' + '<span class="timestamp">' + str + '</span>' + '</li>' + '</ol>');
     	$('#new-message-body').val("");
-    });
+	}
+    
+	$("#new-message-button").on("click", send);
 
+	// $("#conversation a").on("click", function(){
+ //    	console.log("X was clicked");
+ //    });
 
 });
