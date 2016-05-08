7e0355fb0ba90482a1948a1ac9b33ddcbf40cd05
Rogerio Istiraneopulos <Rogerio@Rogerios-iMac.local>
2016-05-08 16:12:34 -0400

    added display of time

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
     
 	$("#new-message-button").on("click", function(){
-         var msg = $("#new-message-body").val();
+		var time = (new Date);
+		var h = time.getHours();
+		var m = (time.getMinutes()<10?'0':'') + time.getMinutes()
+		var str = h + ":" + m
+        var msg = $("#new-message-body").val();
     	// alert(msg);
-    	$(".chat").append('<ol id="conversation">' + '<li class="message">' + '<a class="delete" href="#">Delete</a>' + '<h3 class="author">Me</h3>' + '<p class="message-body">' + msg + '</p>' + '<span class="timestamp">01:12</span>' + '</li>' + '</ol>');
+    	$(".chat").append('<ol id="conversation">' + '<li class="message">' + '<a class="delete" href="#">Delete</a>' + '<h3 class="author">Me</h3>' + '<p class="message-body">' + msg + '</p>' + '<span class="timestamp">' + str + '</span>' + '</li>' + '</ol>');
     	$('#new-message-body').val("");
     });
 
